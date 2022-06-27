477e57c0ddb6a046ede364fe678d7b30acd76b42
Balara <balarajayaweera@gmail.com>
2022-06-28 02:22:34 +0530

    added components to add category page

--- src/pages/owner/AddCategoryPage/index.jsx
+++ src/pages/owner/AddCategoryPage/index.jsx
@@ -1,26 +1,86 @@
-import { FormGroup, TextField } from "@mui/material";
+import { Box, Checkbox, FormControlLabel, FormGroup } from "@mui/material";
 import React from "react";
+import { PRIMARY1_COLOR } from "../../../theme/colors";
+import TitleText from "../../../components/Owner/TitleText";
+import bg_category from "../../../assets/bg_cat.svg";
+import AddButton from "../../../components/Owner/AddButton";
+import { Formik } from "formik";
 
 function AddCategory() {
-  return (
-    <div>
-      <FormGroup>
-        <TextField
-          id="outlined-basic"
-          label="Category Title"
-          variant="outlined"
-          // value={}
-          // onChange={}
+  const [categoryTitle, setCategoryTitle] = React.useState("");
+  const [subCategoryCheck, setSubCategoryCheck] = React.useState(false);
 
-          sx={{
-            marginBottom: 3,
-            width: 600,
-            borderColor: "red",
-          }}
-          InputProps={{ style: { borderColor: "red" } }}
-        />
-      </FormGroup>
-    </div>
+  React.useEffect(() => {
+    console.log("title: " + categoryTitle);
+    console.log("checked: " + subCategoryCheck);
+  });
+
+  const handleCategoryTitle = (event) => {
+    setCategoryTitle(event.target.value);
+  };
+
+  const handleSubCategoryCheck = (event) => {
+    setSubCategoryCheck(event.target.checked);
+  };
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        width: "100%",
+        height: "100%",
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          width: "100%",
+          height: "100%",
+          position: "absolute",
+          top: "15%",
+        }}
+      >
+        <FormGroup>
+          <TitleText
+            label="Category Title"
+            onChange={handleCategoryTitle}
+            value={categoryTitle}
+            required
+          />
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={subCategoryCheck}
+                onChange={handleSubCategoryCheck}
+                sx={{
+                  color: PRIMARY1_COLOR,
+                  // "&.Mui-checked": {
+                  //   color: PRIMARY1_COLOR,
+                  // },
+                }}
+              />
+            }
+            label="Sub Categories"
+          />
+
+          <AddButton />
+        </FormGroup>
+      </Box>
+      <img
+        src={bg_category}
+        style={{
+          width: 496,
+          height: 325,
+          position: "absolute",
+          bottom: "3%",
+          left: "20%",
+        }}
+      />
+    </Box>
   );
 }
 
